8f58db3ee1e4d4ae381e241cc77a369992e16438
JeffJeong <jbw140@gmail.com>
2024-01-12 17:15:54 +0900

    fix: Rename Function add to adder

--- adder.py
+++ adder.py
@@ -1,11 +1,11 @@
 #adder function
 #return sum of 2 numbers
 
-def add(a,b):
+def adder(a,b):
     return a+b
 
 if __name__=='__main__':    
     a = int(input("Input a number : "))
     b = int(input("Input a number : "))
-    print ("Sum : ",add(a,b))
+    print ("Sum : ",adder(a,b))
 
